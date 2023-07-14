67d3838af59b1bf6d9c2bd2b96d35dae32ee1890
Fidan Bashirova <fidanbb@code.edu.az>
2023-07-14 19:09:02 +0400

    added classWork and homework

---
+++ ClassWorkMethods/ClassWorkMethods/Program.cs
@@ -0,0 +1,252 @@
+﻿
+#region ClassWork
+
+//ShowDates();
+//static void ShowDates()
+//{
+//    Welcome();
+//    string fullName = "Kubra Memmedova";
+//    string email = "kubra@code.edu.az";
+//    string fullData = $"{fullName} - {email}";
+//    ShowAge();
+//    Console.WriteLine(fullName);
+//}
+
+//static void ShowAge()
+//{
+//    Welcome();
+//    int age = 25;
+//    Console.WriteLine(age);
+//}
+
+//static void Welcome()
+//{
+//    Console.WriteLine("Welcome Kubra");
+//}
+
+
+
+//static void SumItemsOfArray()
+//{
+//    int [] arr = {1,2,3,4,5 };
+//    int sum = 0;
+//    foreach (var item in arr)
+//    {
+//        sum += item;
+//    }
+//    Console.WriteLine(sum); ;
+//}
+
+//SumItemsOfArray();
+
+
+
+
+
+//static void SearchByName()
+//{
+//    string[] people = { "Sheref", "Pervin", "Cahangir", "Huseyn" };
+//    string searchText = "Pervinnn";
+//    foreach (var person in people)
+//    {
+//        if(person == searchText)
+//        {
+//            Console.WriteLine(person);
+//            break;
+//        }
+//        if(person == "Cahangir")
+//        {
+//            Console.WriteLine(person);
+//           return;
+//        }
+//    }
+//    Console.WriteLine("Our search method is working ");
+//}
+
+//SearchByName();
+
+
+
+
+//static void Sum(int number1, int number2)
+//{
+//    int sum = number1 + number2;
+//    Console.WriteLine(sum);
+//}
+
+//Sum(5,9);
+
+
+
+//bool isMarriad = false;
+
+//static void ShowText(string text, int num)
+//{
+//    Console.WriteLine(text + "-"+ num);
+//}
+//if (isMarriad)
+//{
+//    Console.WriteLine("Evlidir");
+//}
+
+//string str = "Hello P139";
+//int number = 55;
+//ShowText(str, number);
+
+
+
+//static void Login(string username,string password)
+//{
+//    if (username == "samir555" && password == "Samir123_")
+//    {
+//        Console.WriteLine("Login successfull");
+//    }
+//    else
+//    {
+//        Console.WriteLine("Username or password is wrong");
+//    }
+//}
+
+//Console.WriteLine("Add your username: ");
+//string username = Console.ReadLine();
+
+//Console.WriteLine("Add your password: ");
+//string password = Console.ReadLine();
+
+//Login(username, password);
+
+
+
+//Console.WriteLine("Add your name");
+
+//string name = Console.ReadLine();
+
+//Console.WriteLine(name);
+
+
+//Console.WriteLine("Add first number: ");
+//int number1 = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("Add second number: ");
+//int number2 = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("--------------------");
+
+//SumNumbers(number1, number2);
+
+
+//static void SumNumbers(int a,int b)
+//{
+//    int sum = a + b;
+//    Console.WriteLine(sum);
+//}
+
+
+
+//static string ShowName()
+//{
+//    return "Samir";
+//}
+
+//var text = "sdsd";
+
+//string result = ShowName();
+//Console.WriteLine(ShowName());
+
+
+
+//static void Sum(int a,int b)
+//{
+//    Console.WriteLine(a+b);
+//}
+
+
+
+
+
+//static int Sum(int a,int b)
+//{
+//    return a + b;
+//}
+
+//int num1 = 5;
+//int num2 = 100;
+//int result =Sum(num1,num2);
+
+
+//Console.WriteLine(result);
+
+
+
+//static int GetSumOfNumbersFromArray(int[] numbers)
+//{
+//    int sum = 0;
+
+//    foreach (var item in numbers)
+//    {
+//        sum += item;
+//    }
+//    return sum;
+//}
+
+
+//int[] arr = { 1, 2, 3 };
+
+//int result = GetSumOfNumbersFromArray(arr);
+
+//Console.WriteLine(result);
+
+
+//static bool CheckWeekDayIfSunday(string weekDay)
+//{
+//    bool isSunday = false;
+
+//    switch (weekDay)
+//    {
+//        case "Saturday":
+//            isSunday = false;
+//            break;
+//        case "Monday":
+//            isSunday = false;
+//            break;
+//        case "Tuesday":
+//            isSunday = false;
+//            break;
+//        case "Sunday":
+//            isSunday = true;
+//            break;
+//        case "Thursday":
+//            isSunday = false;
+//            break;
+//        case "Friday":
+//            isSunday = false;
+//            break;
+//        case "Wednesday":
+//            isSunday = false;
+//            break;
+//    }
+//    return isSunday;
+//}
+
+
+//bool result = CheckWeekDayIfSunday("Sunday");
+
+//ShowWeekDay(result);
+
+//static void ShowWeekDay(bool isSunday)
+//{
+//    if (isSunday)
+//    {
+//        Console.WriteLine("Yes, today is Sunday");
+//    }
+//    else
+//    {
+//        Console.WriteLine("No, today is not Sunday");
+
+//    }
+
+//}
+
+
+
+#endregion
---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,340 @@
+﻿
+#region 2. Verilmiş n - ədədinin 3-ə və 7-ə bölünüb-bölünməməsini tapın.
+
+//static void FindDividedOrNot(int n)
+//{
+//    if (n % 3 == 0 && n % 7 == 0)
+//    {
+//        Console.WriteLine("bolunur");
+
+//    }
+
+//    else
+//    {
+//        Console.WriteLine("bolunmur");
+//    }
+//}
+//Console.WriteLine("Enter a value: ");
+
+//int number = Convert.ToInt32(Console.ReadLine());
+
+//FindDividedOrNot(number);
+
+#endregion
+
+#region 3. n və m ədədləri verilir. Əgər n və m ədədləri hər ikisi cütdürsə n və m ədədlərinin cəmini hesablayın.
+
+//static int FindSum(int n,int m)
+
+//{
+//    int sum = 0;
+
+//    if (n % 2 == 0 && m % 2 == 0)
+//    {
+//        sum = n + m;
+//    }
+
+
+//    return sum;
+//}
+
+//Console.WriteLine("Enter first number");
+//int number1 = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("Enter second number");
+//int number2 = Convert.ToInt32(Console.ReadLine());
+
+//int sum = FindSum(number1, number2);
+//Console.WriteLine(sum);
+
+#endregion
+
+#region 4. Verilmis n ve m (n<m) ededleri arasindaki tek ededlerin cemini tapin.
+
+//static int FindSumOfOddNumbers(int n, int m)
+//{
+//    int sum = 0;
+
+
+//    for (int i = n; i <= m; i++)
+//    {
+//        if (i % 2 == 1)
+//        {
+//            sum += i;
+//        }
+//    }
+
+
+//    return sum;
+//}
+
+//Console.WriteLine("Enter first number");
+//int n = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("Enter second number");
+//int m = Convert.ToInt32(Console.ReadLine());
+
+//while (n > m)
+//{
+//    Console.WriteLine("Second number must be greater than first number");
+//    Console.WriteLine("Enter first number again");
+//    n = Convert.ToInt32(Console.ReadLine());
+
+//    Console.WriteLine("Enter second number again");
+//    m = Convert.ToInt32(Console.ReadLine());
+
+//}
+
+//int sum = FindSumOfOddNumbers(n, m);
+//Console.WriteLine($"Sum of odd numbers between {n} and {m} is equal to {sum}");
+
+#endregion
+
+#region 5. Verilmish arrayin icindeki tek ededlerin cemini tapin.
+
+//static int SumOfOddArrayElements(int[] nums)
+//{
+//    int sum = 0;
+//    foreach (var num in nums)
+//    {
+//        if (num % 2 == 1)
+//        {
+//            sum += num;
+//        }
+//    }
+//    return sum;
+//}
+
+//int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+
+//int sum =SumOfOddArrayElements(arr);
+
+//Console.WriteLine(sum);
+
+
+#endregion
+
+#region 6. Verilmish arrayin icindeki cut ededlerin sayini tapin.
+
+//static int CountEvenArrayElements(int[] nums)
+//{
+//    int count = 0;
+//    foreach (var num in nums)
+//    {
+//        if (num % 2 == 0)
+//            count++;
+//    }
+//    return count;
+//}
+
+//int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+
+//int count = CountEvenArrayElements(arr);
+
+//Console.WriteLine(count);
+
+
+#endregion
+
+#region 7. Verilmis n ve m (n<m) ededleri arasindaki tek ededlerin sayini tapin.
+
+//static int CountOddNumbers(int n, int m)
+//{
+//    int count = 0;
+
+
+//    for (int i = n; i <= m; i++)
+//    {
+//        if (i % 2 != 0)
+//        {
+//            count ++;
+//        }
+//    }
+
+
+//    return count;
+//}
+
+//Console.WriteLine("Enter first number");
+//int n = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("Enter second number");
+//int m = Convert.ToInt32(Console.ReadLine());
+
+//while (n > m)
+//{
+//    Console.WriteLine("Second number must be greater than first number");
+//    Console.WriteLine("Enter first number again");
+//    n = Convert.ToInt32(Console.ReadLine());
+
+//    Console.WriteLine("Enter second number again");
+//    m = Convert.ToInt32(Console.ReadLine());
+
+//}
+
+//int count = CountOddNumbers(n, m);
+//Console.WriteLine(count);
+
+
+#endregion
+
+#region 8. Verilmis n ve m (n<m) ededleri arasindaki cut ededlerin cemini tapin.
+
+//static int SumOfEvenNumbers(int n, int m)
+//{
+//    int sum = 0;
+
+
+//    for (int i = n; i <= m; i++)
+//    {
+//        if (i % 2 == 0)
+//        {
+//            sum += i;
+//        }
+//    }
+
+
+//    return sum;
+//}
+
+//Console.WriteLine("Enter first number");
+//int n = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("Enter second number");
+//int m = Convert.ToInt32(Console.ReadLine());
+
+//while (n > m)
+//{
+//    Console.WriteLine("Second number must be greater than first number");
+//    Console.WriteLine("Enter first number again");
+//    n = Convert.ToInt32(Console.ReadLine());
+
+//    Console.WriteLine("Enter second number again");
+//    m = Convert.ToInt32(Console.ReadLine());
+
+//}
+
+//int sum = SumOfEvenNumbers(n, m);
+//Console.WriteLine($"Sum of even numbers between {n} and {m} is equal to {sum}");
+#endregion
+
+#region 9. Verilmis n ve m (n<m) ededleri arasindaki cut ededlerin sayini tapin.
+//static int CountEvenNumbers(int n, int m)
+//{
+//    int count = 0;
+
+
+//    for (int i = n; i <= m; i++)
+//    {
+//        if (i % 2 == 0)
+//        {
+//            count++;
+//        }
+//    }
+
+
+//    return count;
+//}
+
+//Console.WriteLine("Enter first number");
+//int n = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("Enter second number");
+//int m = Convert.ToInt32(Console.ReadLine());
+
+//while (n > m)
+//{
+//    Console.WriteLine("Second number must be greater than first number");
+//    Console.WriteLine("Enter first number again");
+//    n = Convert.ToInt32(Console.ReadLine());
+
+//    Console.WriteLine("Enter second number again");
+//    m = Convert.ToInt32(Console.ReadLine());
+
+//}
+
+//int count = CountEvenNumbers(n, m);
+//Console.WriteLine(count);
+
+
+#endregion
+
+#region 10. Verilmis n tam ededinin sade ve ya murekkeb oldugunu tapin.
+
+//static int FindPrimeOrCompositeNum(int n)
+//{
+//    int count = 0;
+//    if (n <= 0)
+//    {
+//        Console.WriteLine("Enter correct number");
+
+//    }
+//    else if (n == 1)
+//    {
+//        Console.WriteLine($"{n} is not prime or composite number.");
+//    }
+//    else
+//    {
+//        for (int i = 1; i <= n; i++)
+//        {
+//            if (n % i == 0)
+//                count++;
+//        }
+//    }
+//    return count;
+//}
+
+//Console.WriteLine("Enter a number");
+//int n = Convert.ToInt32(Console.ReadLine());
+
+//int count = FindPrimeOrCompositeNum(n);
+
+//if (count > 2)
+//{
+//    Console.WriteLine($"{n} is composite number.");
+//}
+//else if (count == 2)
+//{
+//    Console.WriteLine($"{n} is prime number.");
+
+//}
+#endregion
+
+#region 11. n ededinin faktorialini hesablamaq.
+
+
+//static  int FindFactorial(int n)
+//{
+//    int fact = 1;
+//    for (int i=1;i<=n;i++)
+//    {
+//        fact *= i;
+//    }
+//    return fact;
+//}
+
+//Console.WriteLine("Enter a number");
+//int n = Convert.ToInt32(Console.ReadLine());
+//int fact = 1;
+
+//if (n < 0)
+//{
+//    Console.WriteLine("Negative number does not have factorial");
+//}
+
+//else if (n == 0)
+//{
+//    Console.WriteLine($"Factorial of {n} is equal to {fact}");
+//}
+
+//else
+//{
+//    fact = FindFactorial(n);
+//    Console.WriteLine($"Factorial of {n} is equal to {fact}");
+//}
+
+
+
+#endregion
+
+
